fa92ac12e047d3bc374105582f3158455ebde633
smithy98 <smithy_98@outlook.com>
2020-04-14 10:33:51 +0100

    start

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,30 @@
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || "development";
 
 const baseConfig = {
-  client: 'pg',
+  client: "pg",
   migrations: {
-    directory: './db/migrations'
+    directory: "./db/migrations",
   },
   seeds: {
-    directory: './db/seeds'
-  }
+    directory: "./db/seeds",
+  },
 };
 
 const customConfig = {
   development: {
     connection: {
-      database: 'nc_news'
-      // user,
-      // password
-    }
+      database: "nc_news",
+      user: "dan",
+      password: "1234",
+    },
   },
   test: {
     connection: {
-      database: 'nc_news_test'
-      // user,
-      // password
-    }
-  }
+      database: "nc_news_test",
+      user: "dan",
+      password: "1234",
+    },
+  },
 };
 
 module.exports = { ...customConfig[ENV], ...baseConfig };
