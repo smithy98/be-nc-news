0f93973a5a3f13a536762da352f7a974f802e20b
smithy98 <smithy_98@outlook.com>
2021-05-25 15:31:56 +0100

    console log in connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,10 @@
 const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
 
+console.log("->", ENV);
 const dbConfig =
   ENV === "production"
     ? { client: "pg", connection: process.env.DATABASE_URL }
     : require("../knexfile");
 
-module.exports = require("knex")(dbConfig);
+module.exports = knex(dbConfig);
