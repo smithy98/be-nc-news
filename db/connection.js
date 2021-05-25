0fbd8b4b6c059257f8136bbae65d5f3a70d97776
smithy98 <smithy_98@outlook.com>
2021-05-25 15:36:25 +0100

    console log in connection.js2

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
 
-console.log("->", ENV);
+console.log("->", process.env.DATABASE_URL);
 const dbConfig =
   ENV === "production"
     ? { client: "pg", connection: process.env.DATABASE_URL }
